4808ed9d521922f4329a8b663d56e509db025319
shahboz <shahbozmamatkarimov2303@gmail.com>
2023-04-27 11:01:55 +0300

    dashboard

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { useRoute } from "vue-router";
 import store from './store'
 
 import './assets/main.css'
@@ -8,6 +9,7 @@ import './assets/main.css'
 const app = createApp(App)
 
 app.use(router)
+useRoute().push('/')
 app.use(store)
 
 app.mount('#app')
